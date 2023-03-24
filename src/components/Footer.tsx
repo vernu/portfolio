2cef1896ee48fb3b1435b919a58eeb4964203122
isra el <vernu1997@gmail.com>
2023-03-24 22:00:14 +0300

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='bg-gray-800 px-6 py-8 text-center text-white'>
-      &copy; {new Date().getFullYear()}
+      &copy; {new Date().getFullYear()} No rights reserve :)
     </footer>
   )
 }
