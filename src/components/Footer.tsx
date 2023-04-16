a2007165c6f7fa27649d11b634f3d42fadfbecfb
isra el <vernu1997@gmail.com>
2023-04-17 02:22:00 +0300

    fix typo in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='bg-gray-800 px-6 py-8 text-center text-white'>
-      &copy; {new Date().getFullYear()} No rights reserve :)
+      &copy; {new Date().getFullYear()} No rights reserved :)
     </footer>
   )
 }
