8433f2968b9e92e088d025c00fd350dfe751834f
isra el <vernu1997@gmail.com>
2023-04-23 12:10:36 +0300

    add meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,14 @@ import AboutSection from '@/components/AboutSection'
 import ExperienceSection from '@/components/ExperienceSection'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import { Meta } from '@/components/Meta'
 import PortfolioSection from '@/components/PortfolioSection'
 import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className='bg-gray-100 min-h-screen'>
-      <Head>
-        <title>Israel Abebe</title>
-      </Head>
-
+      <Meta />
       <Header />
       <AboutSection />
       <PortfolioSection />
